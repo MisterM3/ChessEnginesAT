ca5a5f9661760f6ab7cbf5436be9cef3a08b8469
MisterM <510249@student.saxion.nl>
2023-03-03 11:47:26 +0100

    Added rook functionallity
    rook can move horizontal and vertical

---
+++ My project (2)/Assets/Rook.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rook : Pieces
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        GameBoard.Instance.SetPieceAtLocation(gridPosition, this);
+
+
+        moveDirections.Add(new Vector2Int(-1, 0));
+        moveDirections.Add(new Vector2Int(1, 0));
+        moveDirections.Add(new Vector2Int(0, -1));
+        moveDirections.Add(new Vector2Int(0, 1));
+    }
+}
