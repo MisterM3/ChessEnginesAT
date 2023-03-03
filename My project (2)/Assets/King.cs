0046fd96c53849c12c572e5f2f97929092da475c
MisterM <510249@student.saxion.nl>
2023-03-03 12:02:35 +0100

    King movement added
    king moves one space in all direction, can still make illegal moves (can put itself in check)

---
+++ My project (2)/Assets/King.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class King : Pieces
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        GameBoard.Instance.SetPieceAtLocation(gridPosition, this);
+
+        maxMoveAmount = 1;
+        moveDirections.Add(new Vector2Int(-1, -1));
+        moveDirections.Add(new Vector2Int(1, 1));
+        moveDirections.Add(new Vector2Int(1, -1));
+        moveDirections.Add(new Vector2Int(-1, 1));
+        moveDirections.Add(new Vector2Int(-1, 0));
+        moveDirections.Add(new Vector2Int(1, 0));
+        moveDirections.Add(new Vector2Int(0, -1));
+        moveDirections.Add(new Vector2Int(0, 1));
+    }
+}
