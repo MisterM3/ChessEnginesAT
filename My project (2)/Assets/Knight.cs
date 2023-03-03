d2520b4981197a2ee332cedf8251f0efe51a5a93
MisterM <510249@student.saxion.nl>
2023-03-03 12:10:49 +0100

    Knight Movement added
    L-shape for knight/horse

---
+++ My project (2)/Assets/Knight.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight : Pieces
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        GameBoard.Instance.SetPieceAtLocation(gridPosition, this);
+
+        maxMoveAmount = 1;
+        moveDirections.Add(new Vector2Int(-1, -2));
+        moveDirections.Add(new Vector2Int(-2, -1));
+        moveDirections.Add(new Vector2Int(-2, 1));
+        moveDirections.Add(new Vector2Int(2, 1));
+        moveDirections.Add(new Vector2Int(2, -1));
+        moveDirections.Add(new Vector2Int(-1, 2));
+        moveDirections.Add(new Vector2Int(1, 2));
+        moveDirections.Add(new Vector2Int(1, -2));
+    }
+}
